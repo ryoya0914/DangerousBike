329d61a4f3cb5c9711140eb5cf9836a1618a04c3
Ryoya0914 <Denya.Adidas@gmail.com>
2017-05-20 14:32:55 +0900

    add UI

---
+++ DengerousBike/Assets/MainGame/Scripts/movescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movescript : MonoBehaviour
+{
+    float spped = 0f;
+    float movePower = 0.2f;
+    Rigidbody2D rb2d;
+
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void Update ()
+    {
+	}
+}
