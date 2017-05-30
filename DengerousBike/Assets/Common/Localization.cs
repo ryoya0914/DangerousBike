1d6c5cecab5b80399369c99e901effa9f37e0945
Rivero Sato <riveroyuichitrabajosdeinforma@gmail.com>
2017-05-30 22:30:23 +0900

    Add LocalizedText system

---
+++ DengerousBike/Assets/Common/Localization.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Localization : MonoBehaviour {
+    private Text m_text;
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+        if(m_text)
+            m_text.text = LocalizedText.Instance.GetWord(LocalizedText.WORDS.ACCEL);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
