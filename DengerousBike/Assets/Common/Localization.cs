b63614565ce1c116e36f7256df35ab6ae3930040
W301-07\student <gundam@gmail.com>
2017-06-01 18:09:52 +0900

    Add completed localization system

--- DengerousBike/Assets/Common/Localization.cs
+++ DengerousBike/Assets/Common/Localization.cs
@@ -1,26 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Localization : MonoBehaviour {
-    private Text m_text;
+public class Localization : Singleton<Localization> {
 
-    void Awake()
+    private int m_currentLanguage = 0;
+    //For playerprefs
+    private string m_languageKey = "currentLanguage";
+
+    public enum LANGUAGES
     {
-        m_text = GetComponent<Text>();
-        if(m_text)
-            m_text.text = LocalizedText.Instance.GetWord(LocalizedText.WORDS.ACCEL);
+        NONE,
+        ENGLISH,
+        JAPANESE,
+        SPANISH,
+        LANGUAGE_MAX
+    };
+
+    struct UIWord
+    {
+        private string english;
+        private string japanese;
+        private string spanish;
+
+        public UIWord(string en, string jp, string es)
+        {
+            english = en;
+            japanese = jp;
+            spanish = es;
+        }
+
+        public string GetWord(LANGUAGES lang)
+        {
+            switch (lang)
+            {
+                case LANGUAGES.ENGLISH:
+                    return english;
+
+                case LANGUAGES.JAPANESE:
+                    return japanese;
+
+                case LANGUAGES.SPANISH:
+                    return spanish;
+            }
+
+            return " ";
+        }
+    };
+
+
+    public enum WORDS
+    {
+        ACCEL,
+        BRAKE
+    };
+
+    private UIWord[] m_words = new UIWord[]
+    {
+        new UIWord("ACCEL", "アクセル", "ACELERAR"),
+        new UIWord("BRAKE", "ブレーキ", "FRENAR")
+    };
+
+	public void Initialize ()
+    {
+        if (m_currentLanguage != (int)LANGUAGES.NONE)
+            return;
+
+        m_currentLanguage = PlayerPrefs.GetInt(m_languageKey, 0);
+
+        if (m_currentLanguage == (int)LANGUAGES.NONE || m_currentLanguage >= (int)LANGUAGES.LANGUAGE_MAX)
+        {
+            SetLanguageToSystem();
+        }
     }
 
+    private void SetLanguageToSystem()
+    {
+        var sysLang = Application.systemLanguage;
+        switch (sysLang)
+        {
+            case SystemLanguage.English:
+                m_currentLanguage = (int)LANGUAGES.ENGLISH;
+                break;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+            case SystemLanguage.Japanese:
+                m_currentLanguage = (int)LANGUAGES.JAPANESE;
+                break;
+
+            case SystemLanguage.Spanish:
+                m_currentLanguage = (int)LANGUAGES.SPANISH;
+                break;
+
+            default:
+                m_currentLanguage = (int)LANGUAGES.ENGLISH;
+                break;
+        }
+
+        PlayerPrefs.SetInt(m_languageKey, m_currentLanguage);
+    }
+
+    public void ChangeLanguage(LANGUAGES lang)
+    {
+        m_currentLanguage = (int)lang;
+    }
+
+    public string GetWord(WORDS word)
+    {
+        Initialize();
+        return m_words[(int)word].GetWord((LANGUAGES)m_currentLanguage);
+    }
 }
---
+++ DengerousBike/Assets/Common/LocalizeText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizeText : MonoBehaviour {
+    private Text m_text;
+    public Localization.WORDS word;
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+        if (m_text)
+            m_text.text = Localization.Instance.GetWord(word);
+    }
+}
