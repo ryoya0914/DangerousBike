c7b94c38811b7a3b8415eef110353825794c31df
Rivero Sato <riveroyuichitrabajosdeinforma@gmail.com>
2017-06-15 22:56:48 +0900

    Add Applovin rewarded video

---
+++ DengerousBike/Assets/Common/AdController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdController : Singleton<AdController>
+{
+    public delegate void RewardAproved();
+
+    RewardAproved rewardAprovedDel = null;
+
+    void Start()
+    {
+        AppLovin.SetSdkKey("cBmxxgR0WEQ2g9uRR0mbcORZUeFHePvFbfAXKcTsrzvW7nzZniAN6cyaFCsucm8DPJllwHbmFUHNFFx0y5O2hL");
+        AppLovin.InitializeSdk();
+        AppLovin.LoadRewardedInterstitial();
+        AppLovin.SetUnityAdListener(gameObject.name);
+    }
+
+    public bool isRewardedVideoReady()
+    {
+        return AppLovin.IsIncentInterstitialReady();
+    }
+
+    public void ShowRewardedVideo()
+    {
+        if(AppLovin.IsIncentInterstitialReady())
+        {
+            AppLovin.ShowRewardedInterstitial();
+        }
+    }
+
+    public void SetRewardAprovedDelegate(RewardAproved rewardDel)
+    {
+        rewardAprovedDel = rewardDel;
+    }
+
+    void onAppLovinEventReceived(string ev)
+    {
+        if(ev.Contains("REWARDAPPROVEDINFO"))
+        {
+            if(rewardAprovedDel != null)
+            rewardAprovedDel();
+        }
+    }
+}
