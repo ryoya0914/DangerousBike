113f4fd7cd49d039eea4eeaa131f86fbc7464a48
Ryoya0914 <Denya.Adidas@gmail.com>
2017-06-15 13:08:57 +0900

    Add Up&down

--- DengerousBike/Assets/MainGame/Scripts/BikeController.cs
+++ DengerousBike/Assets/MainGame/Scripts/BikeController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 public class BikeController : MonoBehaviour
 {
     public bool push = false;
+    public bool WL = false;
     public float speed = 0;
 
     float movePower = 0.2f;
     Rigidbody2D rb2d;
 
-    const int MinLane = -2;
+    const int MinLane = -1;
     const int MaxLane = 2;
     const float LaneWidth = 1.0f;
     int targetLane;
     public GameObject wall;
+
 
 	void Start ()
     {
@@ -42,6 +44,15 @@ public class BikeController : MonoBehaviour
                 rb2d.velocity = new Vector3(speed, rb2d.velocity.y);
             }
         }
+
+        if (WL == true)
+        {
+            Wheelie();
+        }
+        else if (WL == false)
+        {
+
+        }
     }
 
     public void Accel()
@@ -56,13 +67,67 @@ public class BikeController : MonoBehaviour
 
     }
 
-    public void Left()
+    public void Down()
     {
-        if (wall.isStatic && targetLane > MinLane) targetLane--;
+        if (targetLane > MinLane)
+        {
+            ChangeLane(false);
+        }
+
+
+
     }
 
-    public void Right()
+    public void Up()
     {
-        if (wall.isStatic && targetLane > MinLane) targetLane++;
+        if (targetLane < MaxLane)
+        {
+            ChangeLane(true);
+        }
+    }
+
+    void Wheelie()
+    {
+        transform.Rotate(0, 0, 70);
+    }
+
+    public void WheelieUp()
+    {
+        WL = true;
+    }
+    public void WheelieDown()
+    {
+        WL = false;
+    }
+
+    void ChangeLane(bool up)
+    {
+        var inc = up ? 1 : -1;
+        targetLane += inc;
+        var To = wall.transform.position;
+        To.y += inc * 0.8f;
+        StartCoroutine(MoveFormTo(wall.transform, wall.transform.position, To, 1.0f));
+    }
+
+    IEnumerator MoveFormTo(Transform ToMove,Vector3 Form,Vector3 To,float speed)
+    {
+        float step = (speed / (Form - To).magnitude) * Time.fixedDeltaTime;
+        float t = 0;
+        while (t <= 1.0f)
+        {
+            t += step;
+            var newpos = Vector3.Lerp(Form, To, t);
+            var currpos = ToMove.localPosition;
+            currpos.y = newpos.y;
+            currpos.x = 0;
+            ToMove.localPosition = currpos;
+            yield return new WaitForFixedUpdate();
+        }
+
+        var pos1 = Vector3.Lerp(Form, To, t);
+        var pos2 = ToMove.localPosition;
+        pos1.y = pos2.y;
+        pos1.x = 0;
+        ToMove.localPosition = pos1;
     }
 }
