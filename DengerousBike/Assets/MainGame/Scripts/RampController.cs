12b9013e4a754c9730d9c5c9493f4c8d1516602b
Ryoya0914 <Denya.Adidas@gmail.com>
2017-06-28 16:48:37 +0900

    Add scitpts RampController

---
+++ DengerousBike/Assets/MainGame/Scripts/RampController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampController : MonoBehaviour
+{
+    public bool Jump = false;
+    public float flap = 1000f;
+    Rigidbody2D rb2d;
+
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
